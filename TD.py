e8084516ccbdbc439bd927d6614fc016014e83b9
MathurinG <mathurin.gesny@etudiant.univ-rennes2.fr>
2024-10-07 14:17:44 +0200

    on a le contenu d'une page

--- TD.py
+++ TD.py
@@ -1,6 +1,7 @@
 import requests
 
 url = "https://www.bandsintown.com/all-dates/fetch-next/upcomingEvents?page=4&longitude=2.3488&latitude=48.85341"
+header={"User-Agent":"Mozilla/5.0"}
 
-reponse = requests.get(url)
+reponse = requests.get(url,headers=header)
 print(reponse.json())
