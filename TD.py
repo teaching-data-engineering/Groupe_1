4462ce04179cce17f0f94c013edd42e9e693a432
MathurinG <mathurin.gesny@etudiant.univ-rennes2.fr>
2024-10-07 14:24:17 +0200

    avec les params

--- TD.py
+++ TD.py
@@ -1,7 +1,7 @@
 import requests
 
-url = "https://www.bandsintown.com/all-dates/fetch-next/upcomingEvents?page=4&longitude=2.3488&latitude=48.85341"
+url = "https://www.bandsintown.com/all-dates/fetch-next/upcomingEvents?"
 header={"User-Agent":"Mozilla/5.0"}
 
-reponse = requests.get(url,headers=header)
+reponse = requests.get(url,headers=header,params={"page":4,"longitude":"2.3488","latitude":"48.85341"})
 print(reponse.json())
